57105a516b181d877a630cd872e412a3e09f0c74
leavelet <leavelet0@gmail.com>
2021-07-29 10:22:32 +0800

    Xcode init

---
+++ MyCut/MyCut/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyCut
+//
+//  Created by 孙远航 on 2021/7/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyCut/MyCut/MyCutApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCutApp.swift
+//  MyCut
+//
+//  Created by 孙远航 on 2021/7/29.
+//
+
+import SwiftUI
+
+@main
+struct MyCutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
